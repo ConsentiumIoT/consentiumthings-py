0c238bb2fbbb71be3f42e6c66bf48eb19f9ef30e
debjyotiC <djbabai.debjyoti@gmail.com>
2024-02-25 13:02:01 +0530

    updates

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1'
+VERSION = '0.2'
 DESCRIPTION = 'Python API for uploading data to Consentium IoT cloud'
-
+LONG_DESCRIPTION = 'A package that allows to send sensor data to Consentium IoT cloud.'
 
 # Setting up
 setup(
@@ -18,8 +18,10 @@ setup(
     author="Consentium",
     author_email="<consentium.inc@gmail.com>",
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
-    keywords=['python', 'communication', 'internet of things'],
+    keywords=['python', 'sensor', 'internet of things'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
