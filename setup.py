6e8c53b102294ec3f9ddc4f38db2e5be2bd8d155
Consentium IoT <84334208+ConsentiumIoT@users.noreply.github.com>
2024-12-17 14:02:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.5.1'
+VERSION = '0.5.2'
 DESCRIPTION = 'Python lib for sending sensor data to Consentium IoT server.'
 LONG_DESCRIPTION = (
     "A Python library that enables developers to send sensor data to the "
